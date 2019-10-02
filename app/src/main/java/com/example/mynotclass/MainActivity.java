2ffeb81296a07a40017402e1903a991e565f3032
Desktop <m.chavez6@alumnos.santotomas.cl>
2019-10-01 22:43:36 -0300

    trabajo_2.1

--- app/src/main/java/com/example/mynotclass/MainActivity.java
+++ app/src/main/java/com/example/mynotclass/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mynotclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Ir_temas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ir_temas = (Button)FindViewByID(R.id.imageButton_buscador);
+        Ir_temas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Ir_temas = new Intent(MainActivity.this, Temas.class);
+                startActivity(Ir_temas);
+            }
+        });
     }
 }
